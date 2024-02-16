0db1e51ed52eb81bf02d98cb4c12a2c1ab7db47f
Ji Liu <liuji1031@gmail.com>
2024-02-15 21:06:45 -0500

    created slam ekf script

---
+++ slam_ekf/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'slam_ekf'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ji',
+    maintainer_email='liuji1031@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
