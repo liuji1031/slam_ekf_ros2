7106d74844aa7ad0b890e8261651cf944b307698
Ji Liu <liuji1031@gmail.com>
2024-02-17 16:11:42 -0500

    updated gazebo files

--- slam_ekf/setup.py
+++ slam_ekf/setup.py
@@ -1,16 +1,43 @@
 from setuptools import setup, find_packages
+import os
 
 package_name = 'slam_ekf'
+
+def package_files(data_files, directory_list):
+
+    paths_dict = {}
+
+    for directory in directory_list:
+
+        for (path, directories, filenames) in os.walk(directory):
+
+            for filename in filenames:
+
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+
+data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ]
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=package_files(data_files, ['model/', 'launch/', 'worlds/']),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ji',
